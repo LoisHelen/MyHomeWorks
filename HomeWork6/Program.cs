9cc6a64a6d85afd6ce3e12e149352f07ac3b8e65
Helen <helenasbots@gmail.com>
2023-01-20 21:18:54 +0500

    homework 6 is correct

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -21,12 +21,15 @@ Console.WriteLine("Количество положительных чисел = 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 /*
-double CrossPoint(double k1, double b1, double k2, double b2)
+double CrossPointX(double k1, double b1, double k2, double b2)
 {
     double x = (b2 - b1) / (k1 - k2);
-    double y = k1 * x + b1;
-    
     return x;
+}
+
+double CrossPointY(double k1, double b1, double x)
+{
+    double y = k1 * x + b1;
     return y;
 }
 
@@ -39,7 +42,7 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите значение b2 для прямой 2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 
-double x = CrossPoint(k1, b1, k2, b2);
-double y = CrossPoint(k1, b1, k2, b2);
+double x = CrossPointX(k1, b1, k2, b2);
+double y = CrossPointY(k1, b1, x);
 Console.WriteLine($"Координаты точки пересечения двух прямых = ({x};{y})");
 */
